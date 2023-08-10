ad3af350e02749a4f98140ab3f8c0aa1b012c555
LUCAS PEREIRA DE SOUZA <lucas.psouza27@senacsp.edu.br>
2023-08-10 17:17:33 -0300

    Update

--- src/main/java/br/com/paperbook/controller/CargoController.java
+++ src/main/java/br/com/paperbook/controller/CargoController.java
@@ -1,5 +1,18 @@
 package br.com.paperbook.controller;
 
-public class CargoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.paperbook.repository.CargoRepository;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/v1/Cargo")
+public class CargoController {
+	
+	@Autowired CargoRepository cargo;
+	
+	
 }
