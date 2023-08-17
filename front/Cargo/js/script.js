70235e1bd07a0c5ff0581aa92a544e207c330a22
LUCAS PEREIRA DE SOUZA <lucas.psouza27@senacsp.edu.br>
2023-08-17 17:24:36 -0300

    fix:update

--- front/Cargo/js/script.js
+++ front/Cargo/js/script.js
@@ -1,13 +1,26 @@
 //Fazer depois
+
+let btncadastrar = document.getElementById("btncadastrar")
+let idcat = "";
+let dados = ""
+
 let titulo = document.getElementById("titulo")
 let salario = document.getElementById("salario")
 let beneficio = document.getElementById("beneficio")
 let cargahoraria = document.getElementById("cargahoraria")
 
-function exibircargos(){
-    let dados="";
-
+function exibirCargo(){
     fetch("http://10.26.45.31:5000/api/v1/cargo/listar")
+    .then((Response)=>Response.json())
+    .then((rs)=>{
+        rs.map((itens,ix)=>{
+            dados=>
+            ``
+        })
+
+    })
+    }
+    
     .then((Response)=>Response.json())
     .then((rs)=>{
 
@@ -20,4 +33,27 @@ function exibircargos(){
             <li>${itens.cargahoraria}</li>
         </ul>`})
     })
+}
+
+function cadastrarCargo(){
+    let dados="";
+
+    fetch("http://10.26.45.31:5000/api/v1/cargo/cadastrar", {
+        method:"POST",
+        headers:{
+            "accept":"application/json",
+            "content-type":"application/json"
+        },
+        body:JSON.stringify({
+            titulo:titulo.value,
+            salario:salario.value,
+            beneficio:beneficio.value,
+            cargahorario:cargahoraria.value
+        })
+        .then((Response)=>Response.json())
+        .then((dados)=>alert(dados))
+        .catch((error)=>console.error(error))
+
+    })
+    
 }
